951b80071d3c87c8ea1d11c90281584c6119fd94
Mahmoud Tawfeek <mahmoudtawfeek815@gmail.com>
2025-08-20 17:45:52 +0300

    up

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -39,7 +39,7 @@ export default function Projects() {
     },
     {
       title: "E-Commerce Angular Website",
-      img: "/angular_ECommerce.png",
+     img: "/angular_ECommerce.png",
       desc: "E_Commerce website by Angular and bootstrap.",
       demo: "#",
       code: "https://github.com/MahmoudFathyTawfeek/Angular_E_commerce",
